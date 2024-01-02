620c9ebf5ca3a3688286af3bed4ab996356b921f
Huzaifa77 <mohammedzaif@gmail.com>
2024-01-02 18:17:26 +0530

    Added the new example for multiple decorators example

--- src/decorator_learning_example1.ts
+++ src/decorator_learning_example1.ts
@@ -2,7 +2,11 @@
 //we do this add to have our own code and can be used(resued) with out chnaging the core functionality of the doSomething function
 //like in this example for say i want to log some addition info like to log which function is executing before if gets executed everytime
 
-function logThis(target: Object, key: string, descriptor: PropertyDescriptor) {
+export function logThis(
+  target: Object,
+  key: string,
+  descriptor: PropertyDescriptor
+) {
   //The three variables are defined as follows
   // target: Here target is an object containing the method being decorated
   // key :Key is the name of the method
@@ -10,16 +14,22 @@ function logThis(target: Object, key: string, descriptor: PropertyDescriptor) {
 
   const originalMethod = descriptor.value; //here originalMethod("somestring") will the same result as doSomethinf("some string does") as this is called before .apply method
 
+  //create a new function and assign that to the descriptor.value and inside this new function we can execute the originalMethod with the arguments as well as customize and finall return the descriptor with our new changes
+
+  //the descriptor.value is replaced with a new functiion below that acts as a warpper around the origin method
+
   descriptor.value = function (...args: any[]) {
     console.log(`Logging before method call:${key} and ${args}`);
+
+    // Assigning to result captures the output for potential use within the decorator and preparation for returning.
     const result = originalMethod.apply(this, args);
+
     console.log(`Logging after method call: ${key} and ${args}`);
+
+    // The result of the original method call is stored in the result variable and then returned from the new function. This ensures that the decorator doesn’t interfere with the original method's behavior but instead augments it by providing additional functionality (logging in this case).
     return result;
   };
-  console.log(
-    "🚀 ~ file: decorator_learning.ts:23 ~ logThis ~ descriptor:",
-    descriptor
-  );
+
   return descriptor;
 }
 
@@ -36,12 +46,6 @@ newLogger.doSomething("passed arg");
 
 //output
 
-// 🚀 ~ file: decorator_learning.ts:23 ~ logThis ~ descriptor: {
-//   value: [Function (anonymous)],
-//   writable: true,
-//   enumerable: false,
-//   configurable: true
-// }
 // Logging before method call:doSomething and passed arg
 // 🚀 ~ Logger_service ~ doSomething ~ msg: passed arg
 // Logging after method call: doSomething and passed arg
---
+++ src/decorator_learning_example2.ts
@@ -0,0 +1,46 @@
+import { logThis } from "./decorator_learning_example1";
+
+export function decorator2(
+  target: Object,
+  key: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    console.log("🚀 new logger before ", key);
+
+    const result = originalMethod.apply(this, args);
+
+    console.log("🚀 new logger after ", key);
+
+    return result;
+  };
+
+  return descriptor;
+}
+
+export class Logger_service {
+  //In TypeScript or JavaScript, you can apply multiple decorators to a single function or method.
+
+  //Just remember that the decorators are applied from the bottom up (in terms of their placement in the code), meaning the decorator closest to the function declaration (@logThis in this case) will be applied first, followed by the decorator above it (@decorator2).
+
+  @decorator2
+  @logThis
+  doSomething(msg: string) {
+    console.log("🚀 ~ 10000000000000000000", msg);
+  }
+}
+
+const newLogger = new Logger_service();
+newLogger.doSomething("fax");
+
+//output:
+// Logging before method call:doSomething and passed arg
+// 🚀 ~ Logger_service ~ doSomething ~ msg: passed arg
+// Logging after method call: doSomething and passed arg
+// 🚀 new logger before  doSomething
+// Logging before method call:doSomething and fax
+// 🚀 ~ 10000000000000000000 fax
+// Logging after method call: doSomething and fax
+// 🚀 new logger after  doSomething
